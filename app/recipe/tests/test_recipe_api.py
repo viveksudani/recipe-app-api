5fe33c846123f1ccdbe445df28d43c2f322f7c79
Vivek Sudani <viveksudani7@gmail.com>
2021-06-15 19:01:33 +0530

    added the assigned_only feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -1,4 +1,3 @@
-from datetime import timedelta
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
@@ -216,7 +215,7 @@ class PrivateRecipeApiTests(TestCase):
         self.assertIn(serializer1.data, res.data)
         self.assertIn(serializer2.data, res.data)
         self.assertNotIn(serializer3.data, res.data)
-    
+
     def test_filter_recipes_by_ingredients(self):
         """Test returning recipes with specific ingredients"""
         recipe1 = sample_recipe(user=self.user, title='Posh beans on toast')
@@ -235,6 +234,6 @@ class PrivateRecipeApiTests(TestCase):
         serializer1 = RecipeSerializer(recipe1)
         serializer2 = RecipeSerializer(recipe2)
         serializer3 = RecipeSerializer(recipe3)
-        self.assertIn(serializer1.data, res.data)        
+        self.assertIn(serializer1.data, res.data)
         self.assertIn(serializer2.data, res.data)
         self.assertNotIn(serializer3.data, res.data)
